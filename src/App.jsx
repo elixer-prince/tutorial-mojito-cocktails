414d71917ac720856eca48babacf6aed3596db1b
Daniel Leith <elixerprince@gmail.com>
2026-01-25 17:49:55 -0500

    Implement Art Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
 import Cocktails from "./components/Cocktails.jsx";
 import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
-import About from "./components/About.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -14,6 +15,7 @@ const App = () => {
             <Hero />
             <Cocktails />
             <About />
+            <Art />
         </main>
     );
 };
