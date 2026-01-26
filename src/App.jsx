cc4cb69207f7b3a1f7c92d9a638e4df2a9a453d5
Daniel Leith <elixerprince@gmail.com>
2026-01-25 20:12:35 -0500

    Implement Menu section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About.jsx";
 import Art from "./components/Art.jsx";
 import Cocktails from "./components/Cocktails.jsx";
 import Hero from "./components/Hero.jsx";
+import Menu from "./components/Menu.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -16,6 +17,7 @@ const App = () => {
             <Cocktails />
             <About />
             <Art />
+            <Menu />
         </main>
     );
 };
